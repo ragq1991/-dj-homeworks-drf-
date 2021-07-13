06aff27f2d6a29f68bd9c9848074cd2f2553b788
ragq <ragq1991@gmail.com>
2021-07-13 12:11:13 +0600

    debug_toolbar

--- 2.2-databases-2/orm_migrations/school/urls.py
+++ 2.2-databases-2/orm_migrations/school/urls.py
@@ -1,7 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 
 from school.views import students_list
+from website import settings
 
 urlpatterns = [
     path('', students_list, name='students'),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/',
+        include(debug_toolbar.urls)),
+        # For django versions before 2.0:# url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
