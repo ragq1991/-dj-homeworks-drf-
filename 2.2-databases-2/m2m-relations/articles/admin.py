3ba9f71566a08014366823ec0c483a77b3baa22a
ragq <ragq1991@gmail.com>
2021-07-17 01:25:17 +0600

    Доп.проверка на превышения количества главных тэгов(должно быть не более одного)

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -7,17 +7,19 @@ from .models import Article, Tag, Scope
 
 class ScopeInlineFormset(BaseInlineFormSet):
     def clean(self):
-        save_access = False
+        main_tag = 0
         for form in self.forms:
             # В form.cleaned_data будет словарь с данными
             # каждой отдельной формы, которые вы можете проверить
             if form.cleaned_data.get('is_main') is True:
-                save_access = True
+                main_tag += 1
             # вызовом исключения ValidationError можно указать админке о наличие ошибки
             # таким образом объект не будет сохранен,
             # а пользователю выведется соответствующее сообщение об ошибке
-        if save_access is False:
+        if main_tag == 0:
             raise ValidationError('Не указан главный Тэг')
+        if main_tag > 1:
+            raise ValidationError('Главных тэгов указано более одного')
         return super().clean()  # вызываем базовый код переопределяемого метода
 
 
